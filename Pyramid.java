7aeec735029da2bd319a5ba7395ba6107a2bfa91
Alan78268 <alankinau@gmail.com>
2019-02-15 17:13:13 -0700

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,53 @@
+import java.lang.Math.*;
+
+class Pyramid {
+  double l;
+  double w;
+  double h;
+
+  public Pyramid(int l, int w, int h) {
+    this.l = l;
+    this.w = w;
+    this.h = h;
+  }
+
+  public double getVolume(){
+    double volVal;
+    return volVal = (l*w*h)/3;
+  }
+
+  public double getSurfArea(){
+    double SurfVal;
+	//return SurfVal = 2;
+     double p1 = ((l*w));
+     //double p2a = (w/2);
+	 //double p2b = (Math.pow(h,2));
+	 //double p2c = Math.sqrt((p2a+p2b));
+	 double p2 = l * ( Math.sqrt( Math.pow( (w/2),2) + Math.pow(h,2) ) );
+	 //double p3 = w;
+     double p3 = w * ( Math.sqrt( Math.pow( (l/2),2) + Math.pow(h,2) ) );
+	 //System.out.println("p1 =" + p1);
+     //System.out.println("p2a =" + p2a);
+	 //System.out.println("p2b =" + p2b);
+	 //System.out.println("p2c =" + p2c);
+	 //System.out.println("p2 =" + p2);
+     //System.out.println("p3 =" + p3);
+	 //System.out.println("p4 =" + p4);
+     //double f1 = (p1 + p2);
+     //double f2 = (p3 * p4);
+     //System.out.println("f1 =" + f1);
+     //System.out.println("f2 =" + f2);
+
+    //return SurfVal = (l*w)+l)*Math.sqrt((Math.pow((w/2),2)+Math.pow((h),2)+w)*sqrt(Math.pow((l/2),2)+Math.pow((h),2));
+    //return SurfVal = (l*w)+l)*Math.sqrt((Math.pow((w/2),2)+Math.pow((h),2)+w)*sqrt(Math.pow((l/2),2)+Math.pow((h),2));
+    //return SurfVal = (l*w)+l)*Math.sqrt((Math.pow((w/2),2)+Math.pow((h),2)+w)*sqrt(Math.pow((l/2),2)+Math.pow((h),2));
+    //return SurfVal = (l*w)+l)*Math.sqrt((Math.pow((w/2),2)+Math.pow((h),2)+w)*sqrt(Math.pow((l/2),2)+Math.pow((h),2));
+    //return SurfVal = (l*w)+l)*Math.sqrt((Math.pow((w/2),2)+Math.pow((h),2)+w)*sqrt(Math.pow((l/2),2)+Math.pow((h),2));
+    
+	//return (p1*p2);
+	//return(p3*p4);
+	return(p1 + p2 + p3);
+	//return (f1+f2);
+	
+  }
+}
---
+++ ShapeTester.java
@@ -0,0 +1,103 @@
+import java.lang.Math.*;
+
+import java.util.Scanner;
+
+
+
+class ShapeTester {
+
+  public static void main(String args[]) {
+
+    Scanner scanner = new Scanner(System.in);
+    Box box = new Box(0, 0, 0);
+
+    Sphere sphere = new Sphere(0);
+
+    Pyramid pyramid = new Pyramid(0, 0, 0);
+
+    boolean running = true;
+		
+    System.out.println("Please enter shape number, 1 = Box, 2 = Sphere, or 3 = Pyramid?");
+    while(running = true){
+      int shape = scanner.nextInt();
+      switch (shape){
+
+        case 1:
+          System.out.println("Okay, lets calulate a box!");
+
+     	  System.out.println("Enter length:");
+          int bLength = scanner.nextInt();
+          box.l = bLength;
+          //System.out.println("debug bLength = " + bLength);
+          System.out.println("Enter width:");
+          int bWidth = scanner.nextInt();
+          box.w = bWidth;
+          //System.out.println("debug bWidth = " + bWidth);
+          System.out.println("Enter height:");	
+          int bHeight = scanner.nextInt();
+          box.h = bHeight;
+          //System.out.println("debug bHeight = " + bHeight);
+
+          // do final calulation
+          double boxVol = box.getVolume();
+          System.out.println("Box Volume = " + boxVol);
+          double boxSurfArea = box.getSurfArea();
+          System.out.println("Box Surface Area = " + boxSurfArea);
+          System.out.println("Would you like to enter number for another shape?, 1 = Box, 2 = Sphere, or 3 = Pyramid?");
+      
+          break;
+
+        case 2:
+          System.out.println("Okay, lets calulate a sphere!");
+
+     	  System.out.println("Enter radius:");
+          double sRadius = scanner.nextDouble();
+          sphere.r = sRadius;
+          //System.out.println("debug sRadius = " + sRadius);
+
+          //System.out.println("debug PI = " + Math.PI);
+          //System.out.println("debug pow3 = " + Math.pow(sRadius,3));
+
+          // do final calulation
+          double sphereVol = sphere.getVolume();
+          System.out.println("Sphere Volume = " + sphereVol);
+          double sphereSurfArea = sphere.getSurfArea();
+          System.out.println("Sphere Surface Area = " + sphereSurfArea);
+          System.out.println("Would you like to enter number for another shape?, 1 = Box, 2 = Sphere, or 3 = Pyramid?");
+      
+    break;
+      
+        case 3:
+          System.out.println("Okay, lets calulate a pyramid!");
+
+     	  System.out.println("Enter length:");
+          double pLength = scanner.nextInt();
+          pyramid.l = pLength;
+          //System.out.println("debug pLength = " + pLength);
+          System.out.println("Enter width:");
+          double pWidth = scanner.nextInt();
+          pyramid.w = pWidth;
+          //System.out.println("debug pWidth = " + pWidth);
+          System.out.println("Enter height:");	
+          double pHeight = scanner.nextInt();
+          pyramid.h = pHeight;
+          //System.out.println("debug pHeight = " + pHeight);
+
+          // do final calulation
+          double pyramidVol = pyramid.getVolume();
+          System.out.println("Pyramid Volume = " + pyramidVol);
+          double pyramidSurfArea = pyramid.getSurfArea();
+          System.out.println("Pyramid Surface Area = " + pyramidSurfArea);
+          System.out.println("Would you like to enter number for another shape?, 1 = Box, 2 = Sphere, or 3 = Pyramid?");
+      
+          break;
+
+      
+        default :
+          System.out.println( "Not a valid shape.");
+          System.out.println("Please enter number for another shape, 1 = Box, 2 = Sphere, or 3 = Pyramid?");
+          running = false;
+      } // end switch
+    } // end while
+  }// end main
+} // end class
